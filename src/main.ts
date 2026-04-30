d45e033e97a236d5d62c5b4b49c15343b403e550
Aido Nayaka <aidonayaka4@gmail.com>
2026-04-30 09:49:13 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap()
+  .then(() => {
+    console.log('server running');
+  })
+  .catch((error) => {
+    console.error(`An error occurred:`, error);
+  });
